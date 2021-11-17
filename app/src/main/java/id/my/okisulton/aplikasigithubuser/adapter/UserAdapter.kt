b8bfbd01e1c2fd95d808d20aca0071e3541919c2
oexlye08 <os.alimie@gmail.com>
2021-11-17 20:38:02 +0700

    user adapter

--- app/src/main/java/id/my/okisulton/aplikasigithubuser/adapter/UserAdapter.kt
+++ app/src/main/java/id/my/okisulton/aplikasigithubuser/adapter/UserAdapter.kt
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import id.my.okisulton.aplikasigithubuser.databinding.ListUserBinding
 import id.my.okisulton.aplikasigithubuser.model.User
+import android.graphics.drawable.Drawable
+import android.annotation.SuppressLint
 
 /**
  * Created by Oki Sulton on 05/11/2021.
@@ -25,10 +27,22 @@ class UserAdapter(
         ListUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val data = listUser[position]
         holder.binding.tvName.text = data.name
         holder.binding.tvUsername.text = data.username
+
+        val uri = data.avatar // where myresource (without the extension) is the file
+
+        val imageResource: Int = holder.binding.root.context.resources.getIdentifier(uri, null, holder.binding.root.context.packageName)
+
+
+        val res: Drawable = holder.binding.root.context.resources.getDrawable(imageResource)
+        holder.binding.imageView.setImageDrawable(res)
+
+
         Glide.with(holder.binding.root)
             .load(data.avatar)
             .circleCrop()
